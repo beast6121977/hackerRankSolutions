cb805f199e9d16426970dec4592be6a3c57da5f9
beast6121977 <info@fashion-lines.ru>
2024-03-03 13:36:34 +0000

    Update migratory-birds.php

--- Algoritms/Implementation/migratoryBirds/migratory-birds.php
+++ Algoritms/Implementation/migratoryBirds/migratory-birds.php
@@ -1,1 +1,39 @@
+function migratoryBirds($arr) {
+    
+    $result = [];
+    
+    foreach ($arr as $bird_id)
+    {
+        if (isset($result[$bird_id])){
+            $result[$bird_id] += 1;
+        }
+        else
+        {
+            $result[$bird_id] = 1;
+        }
+    }
+    
+    arsort($result);
+    
+    $first = array_key_first($result);
+    $first_value = $result[$first];
+    unset($result[$first]);
+    
+    
+    foreach ($result as $bird_id => $amount)
+    {
+        if ($amount == $first_value && $bird_id < $first)
+        {
+            
+            print_r($bird_id);
+            
+            $first = $bird_id;
+        } else {
+            break;
+        }
+    }
+    
+    return $first;
+    
 
+}
